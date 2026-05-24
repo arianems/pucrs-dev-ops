f4d0ceccf8edfabb6a958f44e31bda2fd3c979a8
ariane maranha <ariane.maranha@gmail.com>
2026-05-24 20:37:12 -0300

    Reduce desired count temporarily

--- terraform/app/app.tf
+++ terraform/app/app.tf
@@ -71,7 +71,7 @@ resource "aws_ecs_service" "app" {
   name            = var.aws_ecs_service
   cluster         = aws_ecs_cluster.main.id
   task_definition = aws_ecs_task_definition.app.arn
-  desired_count   = 2
+  desired_count   = 0
   launch_type     = "FARGATE"
 
   network_configuration {
